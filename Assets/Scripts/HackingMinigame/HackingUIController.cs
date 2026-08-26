eb2db502ed58b399e509c7fdbe8b8c5cfd7bcbb7
Kazu <kazutodeveson@gmail.com>
2026-08-26 13:15:41 +0700

    Finished making dialogue system demo (both cutscenes and gameplay), hacking stratagem minigame demo done

---
+++ Assets/Scripts/HackingMinigame/HackingUIController.cs
@@ -0,0 +1,71 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HackingUIController : MonoBehaviour
+{
+    [Header("UI Elements")]
+    public GameObject panelRoot;
+    public Transform nodesContainer; // Has HorizontalLayoutGroup
+    [SerializeField] private HackingStepNode stepNodePrefab;
+    public TextMeshProUGUI timer;
+    [SerializeField] private Sprite arrowSprite;
+    [SerializeField] HackingMinigameManager gameManager;
+
+    private readonly List<HackingStepNode> _activeNodes = new();
+
+    void Start(){
+        timer.gameObject.SetActive(false);
+        panelRoot.SetActive(false);
+        nodesContainer.gameObject.SetActive(false);
+    }
+
+    void Update(){
+        if (gameManager != null){
+            if (gameManager._remainingTime > 0){
+                timer.text = gameManager._remainingTime.ToString("0.00");
+            }
+        }
+    }
+
+    public void ShowUI(bool isVisible) => panelRoot.SetActive(isVisible);
+
+    // public void UpdateTimer(float normalizedTime)
+    // {
+    //     if (timerSlider != null)
+    //         timerSlider.value = Mathf.Clamp01(normalizedTime);
+    // }
+
+    public void RenderSequence(IReadOnlyList<HackingDirection> sequence)
+    {
+        ClearNodes();
+        for (int i = 0; i < sequence.Count; i++)
+        {
+            HackingStepNode node = Instantiate(stepNodePrefab, nodesContainer);
+            node.Setup(sequence[i], arrowSprite);
+            _activeNodes.Add(node);
+        }
+    }
+
+    public void MarkStepCompleted(int index)
+    {
+        if (index >= 0 && index < _activeNodes.Count)
+            _activeNodes[index].SetCompleted();
+    }
+
+    public void MarkStepFailed(int index)
+    {
+        if (index >= 0 && index < _activeNodes.Count)
+            _activeNodes[index].SetFailed();
+    }
+
+    private void ClearNodes()
+    {
+        foreach (var node in _activeNodes)
+        {
+            if (node != null) Destroy(node.gameObject);
+        }
+        _activeNodes.Clear();
+    }
+}
--- Assets/Scripts/Interactor/Interactor.cs
+++ Assets/Scripts/Interactor/Interactor.cs
@@ -4,12 +4,14 @@ public class InteractableObject : MonoBehaviour
 {
     [Header("Interaction Settings")]
     public string objectName = "realComtutar";
+    [SerializeField] HackingMinigameManager hackingManager;
     [TextArea] public string interactionMessage = "Đã tương tác thành công với thiết bị này!";
 
-    // Hàm này sẽ được gọi khi Player đứng gần nhất và bấm phím F
     public virtual void Interact()
     {
         Debug.Log("Đang tương tác với: " + objectName + " - " + interactionMessage);
-        // TODO: Viết logic mở UI, hack máy, hoặc chạy sự kiện của bạn ở đây
+        if (!hackingManager._isActive){
+            hackingManager.StartMinigame();
+        }
     }
 }
